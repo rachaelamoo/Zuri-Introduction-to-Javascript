a82219280170c2d65dcc18128f4e9988761c43c9
Rachael3 <amoorachael3@gmail.com>
2022-06-24 03:05:36 +0100

    First commit- Profile

--- Main.js
+++ Main.js
@@ -1,3 +1,11 @@
 console.log('Name: Rachael Amoo')
 console.log('Height: 5 foot 6 inches')
 console.log('Country: Nigeria')
+
+
+document.writeln('Name: Rachael Amoo') 
+document.write('<br>')
+document.writeln('Height: 5 foot 6 inches') 
+document.write('<br>')
+document.writeln('Country: Nigeria')
+document.write('<br>')
